b36d0e3576991dc5cc0ea8abb832b48f89d728f2
Abhaya Nigam <abhaya.nigam13@gmail.com>
2021-04-08 14:23:44 +0530

    tkinter basics

---
+++ tkinter basics/06_creating_checkbox.py
@@ -0,0 +1,58 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("655x344")
+
+def get_value():
+    print("It works!\n")
+    print(f"Name:- {nameValue.get()}\nPhone:- {phoneValue.get()}\nGender:- {genderValue.get()}\nEmergency Number:- {emergencyValue.get()}\nPayment Mode:- {payment_modeValue.get()}\nfood Service :- {food_serviceValue.get()}")
+
+#Text for the form.
+name = Label(root,text = "Name ")
+name.grid(row = 1, column = 1)
+
+phone = Label(root,text = "Phone ")
+phone.grid(row = 2, column = 1)
+
+gender = Label(root,text = "Gender ")
+gender.grid(row = 3, column = 1)
+
+emergency = Label(root,text = "Emergency Number ")
+emergency.grid(row = 4, column = 1)
+
+payment_mode = Label(root, text = "Payment Mode ")
+payment_mode.grid(row = 5, column = 1)
+
+#tkinter value to storing enties.
+nameValue = StringVar()
+phoneValue = StringVar()
+genderValue = StringVar()
+emergencyValue = StringVar()
+payment_modeValue = StringVar() 
+food_serviceValue = IntVar()# IntVar because it would be 0 or 1.
+
+#Entries for our form
+
+nameEntry = Entry(root,textvariable = nameValue)
+nameEntry.grid(row = 1, column  = 2)
+
+phoneEntry = Entry(root,textvariable = phoneValue)
+phoneEntry.grid(row = 2, column  = 2)
+
+genderEntry = Entry(root,textvariable = genderValue)
+genderEntry.grid(row = 3, column  = 2)
+
+emergencyEntry = Entry(root,textvariable = emergencyValue)
+emergencyEntry.grid(row = 4, column  = 2)
+
+payment_modeEntry = Entry(root,textvariable = payment_modeValue)
+payment_modeEntry.grid(row = 5, column  = 2)
+
+#making checkbox and packing
+food_service = Checkbutton(root,text = "Check here", variable = food_serviceValue)
+food_service.grid(row =6,column = 2)
+
+button1 = Button(root,text = "Submit",command = get_value)
+button1.grid(row = 7,column =2,pady = 10)
+
+root.mainloop()
