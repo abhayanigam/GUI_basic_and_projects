0f7bfca87bf4ec126b2e450b823c74e73827c117
Abhaya Nigam <abhaya.nigam13@gmail.com>
2021-04-11 18:08:28 +0530

    tkinter basics

---
+++ tkinter basics/09_event_handeling.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+def operation(event):
+    print(f"You clicked on the button at {event.x}, {event.y}")
+
+root = Tk()
+root.title("Events in Tkinter")
+root.geometry("644x334")
+
+widget = Button(root, text='Click me please')
+widget.pack()
+
+widget.bind('<Button-1>', operation)
+widget.bind('<Double-1>', quit)
+root.mainloop()
